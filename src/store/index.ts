d8fb0ab32920b5f9038ebe0cb39256f074086965
DQuinn97 <quinten2407@gmail.com>
2025-03-01 13:14:17 +0100

    Revert "basic store files setup + persistence"
    This reverts commit f6b01676a38e296896c2ee05a121b664f78ea077.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,9 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import cartReducer from "./cartSlice";
-
-const persistConfig = {
-  key: "wgb_root",
-  storage,
-  whitelist: ["cartSlice"],
-};
-
-//ADD NEW REDUCERS / APIS HERE
-const rootReducer = combineReducers({
-  cartSlice: cartReducer,
-});
-//this persist the shopping cart in localStorage in the browser
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const weGotBoard = configureStore({
-  reducer: persistedReducer, //add new reducers/APIs to combineReducer instead
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-      },
-    }).concat(logger),
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 export default weGotBoard;
 export type RootState = ReturnType<typeof weGotBoard.getState>;
 export type AppDispatch = typeof weGotBoard.dispatch;
-export const persistor = persistStore(weGotBoard);
