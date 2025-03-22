a2c942217055b1970cea4a51ea596ab57deb7061
DQuinn97 <quinten2407@gmail.com>
2025-03-22 10:19:37 +0100

    preventDefault onSubmit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,11 +2,19 @@ import style from "../css_modules/searchbar.module.css";
 import { BsSearch } from "react-icons/bs";
 
 const SearchBar = () => {
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  };
   return (
-    <form className={style.search}>
-      <input type="text" placeholder="Search for items..." />
-      <select name="dropdown" id="dropdown">
-        <option value="all">All Categories</option>
+    <form className={style.search} onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Search for items..."
+        name="search"
+        id="search"
+      />
+      <select name="category" id="category">
+        <option value="products">All Categories</option>
         <option value="board">Boardgames</option>
         <option value="card">Cardgames</option>
         <option value="dice">Dicegames</option>
